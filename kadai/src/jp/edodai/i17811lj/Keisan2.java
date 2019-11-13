02943d13f83c54959cad5207f8f1f79d3d8c5d31
k503394469 <k503394469@gmail.com>
2019-11-13 09:10:34 +0900

    課題

--- kadai/src/jp/edodai/i17811lj/Keisan2.java
+++ kadai/src/jp/edodai/i17811lj/Keisan2.java
@@ -1,4 +1,30 @@
 package jp.edodai.i17811lj;
 
+import java.util.Scanner;
+
 public class Keisan2 {
+    public static void main(String[] args) {
+//        System.out.println(jissuu());
+        System.out.println(seisuu());
+    }
+    static String jissuu(){
+        try {
+            Nyuryoku nyuryoku=new Nyuryoku();
+            nyuryoku.setJ1();
+            nyuryoku.setJ2();
+            return "j1+j2="+(nyuryoku.getJ1()+nyuryoku.getJ2())+" j1-j2="+(nyuryoku.getJ1()-nyuryoku.getJ2())+" j1*j2="+(nyuryoku.getJ1()*nyuryoku.getJ2())+" j1/j2="+(nyuryoku.getJ1()/nyuryoku.getJ2());
+        } catch (Exception e) {
+            return "数字を入力してください";
+        }
+    }
+    static String seisuu(){
+        try {
+            Nyuryoku nyuryoku=new Nyuryoku();
+            nyuryoku.setS1();
+            nyuryoku.setS2();
+            return "S1+S2="+(nyuryoku.getS1()+nyuryoku.getS2())+" S1-S2="+(nyuryoku.getS1()-nyuryoku.getS2())+" S1*S2="+(nyuryoku.getS1()*nyuryoku.getS2())+" S1/S2="+(nyuryoku.getS1()/nyuryoku.getS2());
+        } catch (Exception e) {
+            return "数字を入力してください";
+        }
+    }
 }
--- kadai/src/jp/edodai/i17811lj/Nyuryoku.java
+++ kadai/src/jp/edodai/i17811lj/Nyuryoku.java
@@ -1,4 +1,47 @@
 package jp.edodai.i17811lj;
 
+import java.util.Scanner;
+
 public class Nyuryoku {
+    Scanner scanner=new Scanner(System.in);
+    private Integer s1;
+    private Integer s2;
+    private double j1;
+    private double j2;
+
+    public Integer getS1() {
+        return s1;
+    }
+
+    public void setS1() {
+        Integer s1=scanner.nextInt();
+        this.s1 = s1;
+    }
+
+    public Integer getS2() {
+        return s2;
+    }
+
+    public void setS2() {
+        Integer s2=scanner.nextInt();
+        this.s2 = s2;
+    }
+
+    public double getJ1() {
+        return j1;
+    }
+
+    public void setJ1() {
+        double j1=scanner.nextDouble();
+        this.j1 = j1;
+    }
+
+    public double getJ2() {
+        return j2;
+    }
+
+    public void setJ2() {
+        double j2=scanner.nextDouble();
+        this.j2 = j2;
+    }
 }
